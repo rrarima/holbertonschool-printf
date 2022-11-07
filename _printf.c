54fabfc626882657c84161f282e1db43d58e2b8d
bigBadMatt <5707@holbertonstudents.com>
2022-11-07 15:54:26 -0800

    refactored to use subfunction to print - printToScreen

--- _printf.c
+++ _printf.c
@@ -4,6 +4,24 @@
 #include "main.h"
 
 /**
+ * printToScreen - prints a string to standard output
+ * @str: a string to be printed
+ * Return - a count of characters printed
+ */
+int printToScreen (char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 1;
+	}
+	return(i);
+}
+
+/**
  * _printf - function that produces output according to a format.
  * @format: the data type of the output required
  * Return: 0, for now.
@@ -11,18 +29,25 @@
 int _printf(const char *format, ...)
 {
 	va_list strings;
-	int i;
+	int i, k;
 	char *str;
 
 	va_start(strings, format);
 	i = 0;
 	str = format;
-
-	while (str[i] != '\0')
+	/*
+	while (str[k])
 	{
-		_putchar(str[i]);
-		i = i + 1;
-	}
+		if (str[k] == '%' && str[k + 1] == 's')
+		{
+			;
+		}
+	        else if ()
+		{
+			;
+		}
+	}*/
+	printToScreen(str);
 	va_end(strings);
 	return(0);
 }
