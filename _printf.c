ed610c7542fc2e635b35520d8ed0b87c426d657a
bigBadMatt <5707@holbertonstudents.com>
2022-11-06 18:56:48 -0800

    added header statements, brief description and function prototype

--- _printf.c
+++ _printf.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
 
 /**
- *
- *
- *
+ * _printf - 
+ * @format:
+ * Return:
 */
+int _printf(const char *format, ...)
+{
+	return(0);
+}
