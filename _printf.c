6b102dbffa7a8b149c1f9f92161cf48f4ae86454
bigBadMatt <5707@holbertonstudents.com>
2022-11-07 14:50:44 -0800

    wdded functionality to print a string

--- _printf.c
+++ _printf.c
@@ -1,14 +1,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h> /*only temp, will refactor to remove putchar later*/
 #include "main.h"
 
 /**
- * _printf - 
- * @format:
- * Return:
+ * _printf - function that produces output according to a format.
+ * @format: the data type of the output required
+ * Return: 0, for now.
 */
 int _printf(const char *format, ...)
 {
+	va_list strings;
+	int i;
+	char *str;
+
+	va_start(strings, format);
+	i = 0;
+	str = format;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i = i + 1;
+	}
+	va_end(strings);
 	return(0);
 }
