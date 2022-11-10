bddf4725b5b73aad9669b5f6fcd7f5fd77b95779
bigBadMatt <5707@holbertonstudents.com>
2022-11-10 12:07:58 -0800

    Revert "refactored to print last digit no matter what, even if its zero"
    This reverts commit 1590d8fb69fe85abfb4318589db63ed2c99edb15.
    
    my bad

--- _printi.c
+++ _printi.c
@@ -7,10 +7,14 @@
 */
 int _printi(unsigned int n, int count)
 {
-	if (n/10 != 0)
+	if (n != 0)
 	{
-		count = 1 + _printi((n/10), 0);
+		count = 1 + _printi((n / 10), 0);
+		_putchar('0' + (n % 10));
+		return (count);
 	}
-	_putchar((n % 10) + '0');
-	return(1 + count);
+	else
+	{
+		return (count);
+	}
 }
